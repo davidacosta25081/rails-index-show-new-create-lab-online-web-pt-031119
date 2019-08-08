6e63df4e554b6c33ea45fade5bf140560f12c09b
David Acosta <davidacosta25081@yahoo.com>
2019-08-08 05:47:48 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -3,11 +3,13 @@ class CouponsController < ApplicationController
 
   def index
 
+    @coupons = Coupon.all
+
   end
 
 
   def show
-
+    @coupon = Coupon.find(params[:id])
 
   end
 
@@ -20,8 +22,11 @@ class CouponsController < ApplicationController
 
   def create
 
-
-
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to coupon_path(@coupon)
   end
 
 
